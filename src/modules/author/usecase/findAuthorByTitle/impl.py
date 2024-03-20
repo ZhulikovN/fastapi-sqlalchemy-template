e7e54e16a75810883ec744cb911ec024c4964920
nikitazulikov <zhulikovnikita884@gmail.com>
2024-03-20 15:50:23 +0100

    поправил pyproject.toml

--- src/modules/author/usecase/findAuthorByTitle/impl.py
+++ src/modules/author/usecase/findAuthorByTitle/impl.py
@@ -1,9 +1,9 @@
 from pymfdata.common.usecase import BaseUseCase
 from pymfdata.rdb.transaction import async_transactional
 
+from src.modules.author.domain.aggregate.id import AuthorId
 from src.modules.author.infrastructure.query.dto import AuthorDTO
 from src.modules.author.infrastructure.query.uow import AuthorQueryUnitOfWork
-from src.modules.author.domain.aggregate.id import AuthorId
 
 from .command import GetAuthorCommand
 
@@ -17,4 +17,3 @@ class FindAuthorByTitleUseCase(BaseUseCase[AuthorQueryUnitOfWork]):
         # author_id = AuthorId(command.author_id)
         # return await self.uow.repository.fetch_by_id(author_id)
         return await self.uow.repository.fetch_by_id(command.author_id)
-
