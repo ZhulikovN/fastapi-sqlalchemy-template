9ed4ccef71e6168967ef8a6a592ae8619a430123
nikitazulikov <zhulikovnikita884@gmail.com>
2024-03-23 16:27:12 +0100

    default=5432

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ class Settings(BaseSettings):
     DB_USER: str = Field(default="DB_USER", description="Имя пользователя базы данных")
     PASSWORD: str = Field(default="PASSWORD", description="Пароль пользователя базы данных")
     HOST: str = Field(default="HOST", description="Хост базы данных")
-    PORT: int = Field(default=5434, description="Порт подключения к базе данных")
+    PORT: int = Field(default=5432, description="Порт подключения к базе данных")
     API_HOST: str = Field(default="127.0.0.1", description="Хост API")
     API_PORT: int = Field(default=5000, description="Порт API")
 
