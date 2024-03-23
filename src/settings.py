8b1db107563d57998f29795d890413c7723dab95
nikitazulikov <zhulikovnikita884@gmail.com>
2024-03-23 21:08:56 +0100

    изменил переменные

--- src/settings.py
+++ src/settings.py
@@ -2,13 +2,13 @@ from pydantic import BaseSettings, Field
 
 
 class Settings(BaseSettings):
-    DBNAME: str = Field(default="DBNAME", description="Имя базы данных")
-    DB_USER: str = Field(default="DB_USER", description="Имя пользователя базы данных")
-    PASSWORD: str = Field(default="PASSWORD", description="Пароль пользователя базы данных")
-    HOST: str = Field(default="HOST", description="Хост базы данных")
-    PORT: int = Field(default=5432, description="Порт подключения к базе данных")
-    API_HOST: str = Field(default="127.0.0.1", description="Хост API")
-    API_PORT: int = Field(default=5000, description="Порт API")
+    DB_NAME: str = Field(description="Имя базы данных")
+    DB_USER: str = Field(description="Имя пользователя базы данных")
+    DB_PASSWORD: str = Field(description="Пароль пользователя базы данных")
+    DB_HOST: str = Field(description="Хост базы данных")
+    DB_PORT: int = Field(description="Порт подключения к базе данных")
+    API_HOST: str = Field(description="Хост API")
+    API_PORT: int = Field(description="Порт API")
 
     class Config:
         env_file = "../.env"
