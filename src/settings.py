105703b89172e7a676b0d671a45b266370c31519
nikitazulikov <zhulikovnikita884@gmail.com>
2024-03-23 21:43:42 +0100

    поправил settings.py

--- src/settings.py
+++ src/settings.py
@@ -11,7 +11,7 @@ class Settings(BaseSettings):
     API_PORT: int = Field(default=8000, description="Порт API")
 
     class Config:
-        env_file = ".env"
+        env_file = "../.env"
         env_file_encoding = "utf-8"
 
 
